6783bcaa8d4ac1c4a9168e95ad7c4db77190c5f8
jaamong <amypure225@naver.com>
2023-12-20 21:41:10 +0900

    3.3 Events

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-// querySelector - CSS selector
-// CSS selector를 사용해서 class hello를 찾고, 그 안에 있는 h1을 가져와라
-// 동일한 것들이 많은 경우에는, 첫번째 element를 가져온다.
-// 모두 가져오고 싶다면, querySelectorAll 을 사용해야 한다.
+const title = document.querySelector("div.hello:first-child h1");
 
-const title = document.querySelector(".hello h1");
-const titles = document.querySelectorAll(".hello h1");
+function handleTitleClick() {
+    title.style.color = "blue";
+}
 
-
-console.log(title);
-console.log(titles);
+title.addEventListener("click", handleTitleClick);  // click event에 대해서 listen
