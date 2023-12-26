ff08861a83f8cff531a79ce97941cb8da95b9a5f
jaamong <amypure225@naver.com>
2023-12-26 17:08:41 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-function onLoginSubmit(event) { // 따로 space를 만들지 않으면 어떠한 정보도 받지 않음. 공간을 만들어 주면 JS에서 event를 채워줄 것.
-    event.preventDefault(); // 어떤 event의 기본 행동이든지 발생되지 않도록 막는 것 
-    
+function onLoginSubmit(event) { 
+    event.preventDefault(); 
+
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    
-    const username = loginInput.value; 
-    
-    localStorage.setItem("username", username);
+
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);  // form의 submit event 감지
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
